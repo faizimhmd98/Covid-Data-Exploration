1998a6ad6626e86b94b0a631258c65b209c17207
Mohammed Faisal KV <30584761+faizimhmd98@users.noreply.github.com>
2022-08-03 14:47:16 +0530

    Add files via upload

---
+++ PortfolioProject_1- Covid_Data.sql
@@ -0,0 +1,131 @@
+create database portfolioProject;
+use portfolioProject;
+
+select * from portfolioProject.dbo.coviddeath$ 
+where continent is not null
+order by  3,4;
+select * from portfolioProject.dbo.covidvaccinations$ order by  3,4;
+
+select location , date, total_cases,new_cases,total_deaths,population from portfolioProject.dbo.coviddeath$ order by  1,2;
+
+--Total Cases VS Total Deaths
+
+select location , date, total_cases,total_deaths,(total_deaths/total_cases)*100 as DeathRatio
+from portfolioProject.dbo.coviddeath$ 
+where location like '%india%'
+order by  1,2;
+
+--Country with the highest infection rate 
+
+select location ,population, max(total_cases) as HighestInfectionCount,max(total_deaths/total_cases)*100 as PopulationPercentInfected
+from portfolioProject.dbo.coviddeath$ 
+group by location, population
+order by  PopulationPercentInfected desc
+
+-- Countries with the highest death per population
+select location ,max (cast(total_deaths as int))  as TotalDeathCount
+from portfolioProject.dbo.coviddeath$ 
+where continent is not null
+group by location
+order by  TotalDeathCount desc
+
+--By Continent
+select location ,max (cast(total_deaths as int))  as TotalDeathCount
+from portfolioProject.dbo.coviddeath$ 
+where continent is null
+group by location
+order by  TotalDeathCount desc
+
+--Continents wiht the highest death count per population
+select location ,max (cast(total_deaths as int))  as TotalDeathCount
+from portfolioProject.dbo.coviddeath$ 
+where continent is not null
+group by location
+order by  TotalDeathCount desc
+
+--death percentage on global numbers
+select sum(new_cases)as TotalCases,sum(cast(new_deaths as int))as TotalDeaths, sum(cast(new_deaths as int))/sum(new_cases)*100 as DeathPercentage
+from portfolioProject.dbo.coviddeath$ 
+where continent is not null
+--group by date
+order by 1,2;
+
+--Vaccinations
+select * from portfolioProject..coviddeath$ dea
+join portfolioProject..covidvaccinations$ vac
+on dea.location = vac.location and dea.date = vac.date;
+
+--Total population vs Vaccinations
+select dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations
+from portfolioProject..coviddeath$ dea
+join portfolioProject..covidvaccinations$ vac
+on dea.location = vac.location and dea.date = vac.date
+where (dea.continent is not null )
+order by 1,2;
+
+
+select dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations,sum(cast(vac.new_vaccinations as bigint))over(partition by dea.Location order by dea.location,dea.Date)as CumulativePeopleVaccinated
+from portfolioProject..coviddeath$ dea
+join portfolioProject..covidvaccinations$ vac
+on dea.location = vac.location and dea.date = vac.date
+where (dea.continent is not null  )
+order by 2,3;
+
+
+--USE CTE
+
+with PopvsVac ( Continent,Location,Date,Population,CumulativePeopleVaccinated,new_vaccinations)
+as
+(
+select dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations,sum(cast(vac.new_vaccinations as bigint))over(partition by dea.Location order by dea.location,dea.Date)as CumulativePeopleVaccinated
+from portfolioProject..coviddeath$ dea
+join portfolioProject..covidvaccinations$ vac
+on dea.location = vac.location and dea.date = vac.date
+where (dea.continent is not null  )
+--order by 2,3;
+)
+select* from PopvsVac;
+
+with PopvsVac ( Continent,Location,Date,Population,CumulativePeopleVaccinated,new_vaccinations)
+as
+(
+select dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations,sum(cast(vac.new_vaccinations as bigint))over(partition by dea.Location order by dea.location,dea.Date)as CumulativePeopleVaccinated
+from portfolioProject..coviddeath$ dea
+join portfolioProject..covidvaccinations$ vac
+on dea.location = vac.location and dea.date = vac.date
+where (dea.continent is not null  )
+--order by 2,3;
+)
+select*,(CumulativePeopleVaccinated/Population)*100
+from PopvsVac;
+
+--Temp Table
+
+drop table if exists #PercentPopVaccinated
+create table #PercentPopVaccinated
+(
+Continent nvarchar(255),
+Location nvarchar(255),
+Date datetime,
+Population numeric,
+New_Vacciantions numeric,
+CumulativePeopleVaccinated numeric
+)
+insert into #PercentPopVaccinated
+select dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations,sum(cast(vac.new_vaccinations as bigint))over(partition by dea.Location order by dea.location,dea.Date)as CumulativePeopleVaccinated
+from portfolioProject..coviddeath$ dea
+join portfolioProject..covidvaccinations$ vac
+on dea.location = vac.location and dea.date = vac.date
+--where (dea.continent is not null  )
+--order by 2,3;
+select*,(CumulativePeopleVaccinated/Population)*100
+from #PercentPopVaccinated;
+
+--View for visualization
+
+create view PercentPopVaccinated as 
+select dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations,sum(cast(vac.new_vaccinations as bigint))over(partition by dea.Location order by dea.location,dea.Date)as CumulativePeopleVaccinated
+from portfolioProject..coviddeath$ dea
+join portfolioProject..covidvaccinations$ vac
+on dea.location = vac.location and dea.date = vac.date
+where (dea.continent is not null  )
